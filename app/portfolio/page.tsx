0866dd96dd75c7668a16daa310098c4b12af258f
jiminu <jmu417@gmail.com>
2025-09-24 15:22:48 +0900

    npm axios install

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,15 @@
+import axios from 'axios';
+
+const getResumeData = async () => {
+
+    const response = await axios.get('https://raw.githubusercontent.com/jiminu/first_deploy/refs/heads/main/service/resume_general_info_service.json');
+    return fetch('https://raw.githubusercontent.com/jiminu/first_deploy/refs/heads/main/service/resume_general_info_service.json');
+}
+
 export default function PortfolioPage() {
+    
+    const response = getResumeData();
+    console.log(response);    
     return (
         <div>
             <h1>Portfolio Page</h1>
