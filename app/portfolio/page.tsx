f054853a96241428af29c0ac95d9a141f928e194
jiminu <jmu417@gmail.com>
2025-09-25 09:01:31 +0900

    간단한 div style

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -16,7 +16,7 @@ export default async function PortfolioPage() {
     console.log('response: ', response);    
     
     return (
-        <div>
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20" >
             <h1>포트폴리오</h1>
             <p>첫번째 프로젝트: <a href={response.miniproject1}>프로젝트1 주소</a></p>
             <p>첫번째 프로젝트: <a href={response.miniproject2}>프로젝트2 주소</a></p>
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -16,7 +16,7 @@ export default async function ResumePage() {
     console.log('response: ', response);
 
     return (
-        <div>
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <h1>자기소개</h1>
             <p>이름: {response.name}</p>
             <p>깃허브: <a href={response.github}>깃허브 주소</a></p>
